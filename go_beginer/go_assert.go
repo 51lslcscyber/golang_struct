2b99e47c30f6c5c6777d87203513505c5f5f61c6
lslcyberli <2305689151@qq.com>
2022-05-15 12:22:01 +0800

    新方法

--- go_beginer/go_assert.go
+++ go_beginer/go_assert.go
@@ -1,6 +1,6 @@
 /*
 author:鹄思鹄想_bit森
-Time:2022/5/11 22:46
+Time:2022/5/15 12:20
 warning:注意事件，无则可以删去
 */
 package main
@@ -12,8 +12,8 @@ import (
 type PolicyType int32
 
 const (
-	Policy_MIN PolicyType = 0
-	Policy_MAX PolicyType = 1
-	Policy_MID PolicyType = 2
-	Policy_AVG PolicyType = 3
+	PolicyMin PolicyType = 0
+	PolicyMax PolicyType = 1
+	PolicyMid PolicyType = 2
+	PolicyAvg PolicyType = 3
 )
